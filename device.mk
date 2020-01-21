ab61a97369fd33e8097aec2ed5cf31806c19ee1b
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 14:37:22 +0530

    whyred: Add powerhint.json for sdm636
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
    Signed-off-by: Pranaya Deomani <deomanipranaya007@gmail.com>

--- device.mk
+++ device.mk
@@ -367,7 +367,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.whyred-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
